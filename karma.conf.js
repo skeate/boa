0c651b538c933fd42f69bdf74ad9a93e3307cd70
Jonathan Skeate <skeate@gmail>
2015-03-24 17:29:38 -0400

    Set up Travis-CI and Saucelabs

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,50 @@
 // Generated on Tue Mar 17 2015 21:35:02 GMT-0400 (EDT)
 
 module.exports = function(config) {
+  'use strict';
+  var customLaunchers = {
+    chromeWin: {
+      browserName: 'chrome',
+      platform: 'Windows 7'
+    },
+    chromeLinux: {
+      browserName: 'chrome',
+      platform: 'Linux'
+    },
+    chromeOSX: {
+      browserName: 'chrome',
+      platform: 'OS X 10.9'
+    },
+    firefoxWin: {
+      browserName: 'firefox',
+      platform: 'Windows 7'
+    },
+    firefoxLinux: {
+      browserName: 'firefox',
+      platform: 'Linux'
+    },
+    firefoxOSX: {
+      browserName: 'firefox',
+      platform: 'OS X 10.9'
+    },
+    ie10: {
+      browserName: 'internet explorer',
+      version: '10'
+    },
+    ie11: {
+      browserName: 'internet explorer',
+      version: '11'
+    }
+  };
+  for (var l in customLaunchers) {
+    customLaunchers[l].base = 'SauceLabs';
+  }
+  var browsers = ['Chrome'];
+  var reporters = ['clear-screen', 'mocha', 'coverage'];
+  if (process.env.CI === 'true') {
+    browsers = Object.keys(customLaunchers);
+    reporters = ['dots', 'saucelabs'];
+  }
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -23,11 +67,10 @@ module.exports = function(config) {
       '**/*.coffee': ['coffee']
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['clear-screen', 'mocha', 'coverage'],
+    reporters: reporters,
 
     coverageReporter: {
       reporters: [
@@ -39,27 +82,24 @@ module.exports = function(config) {
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: browsers,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+    customLaunchers: customLaunchers
   });
 };
